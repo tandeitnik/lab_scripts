7d88eaeb41f141af8cdcc6b984b4d0e11b0e82df
tandeitnik <69469166+tandeitnik@users.noreply.github.com>
2022-12-23 10:40:59 -0300

    Uploaded files

---
+++ dataAcqTiePie.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import time
+import os
+import libtiepie
+import numpy as np
+from printinfo import *
+from datetime import datetime
+from tqdm import tqdm
+import beepy
+
+reps = 1 #number of repetitions the data is collected - it is different from number of traces
+delay = 10 #time in seconds the script waits beetween each repetition
+
+freq = int(5e5) #sampling frequency [Hz]
+acqTime = 0.25 # total acquisiton time [s]
+N = 20 # number of traces
+rootFolder = r"C:\Users\Labq\Desktop\Daniel R.T\Nova pasta\data\backward-noparticle" #output folder where folders containint the data will be saved
+coupling= "ACV" #coupling type, can be ACV or DCV.
+voltageRange = 1e-3 #oscilloscope range
+autoRange = 1 #if it equals to 1, voltageRange will be ignored and an automatic range will be determined
+gainAutoRange = 1.5 #multiplicative factor that determines the autoRange
+
+experimentDescription = "Partícula pinçada a 10mbar. Experimento para medir o SNR do foward."
+
+#writing experience info txt
+lines = ['Experiment info',
+         '',
+         'Date and time: '+now.strftime("%d/%m/%Y %H:%M:%S"),
+         'Device: TiePie',
+         'Samp. freq.: '+str(freq),
+         'acq. time.: '+str(acqTime),
+         'Num. traces: '+str(N),
+         'Coupling: '+coupling,
+         'Description: '+experimentDescription
+         ]
+
+with open(os.path.join(rootFolder,'experimentInfo.txt'), 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
+
+for rep in range(reps):
+    
+    outputFolder = os.path.join(rootFolder,"rep"+str(rep))
+    os.mkdir(outputFolder)
+    dt = 1/freq
+    recordLength = int(acqTime/dt)
+
+    """tie pie stuff [begin]"""
+    libtiepie.network.auto_detect_enabled = True
+    
+    # Search for devices:
+    libtiepie.device_list.update()
+    
+    # Try to open an oscilloscope with stream measurement support:
+    scp = None
+    for item in libtiepie.device_list:
+        if item.can_open(libtiepie.DEVICETYPE_OSCILLOSCOPE):
+            scp = item.open_oscilloscope()
+            if scp.measure_modes & libtiepie.MM_STREAM:
+                break
+            else:
+                scp = None
+                
+    assert scp != None, "OSCILLOSCOPE NOT FOUND"
+    
+    scp.measure_mode = libtiepie.MM_STREAM
+    
+    # Set sample frequency:
+    scp.sample_frequency = freq
+    
+    # Set record length:
+    scp.record_length = recordLength
+    
+    # For all channels:
+    for ch in scp.channels:
+        # Enable channel to measure it:
+        ch.enabled = True
+    
+        # Set range:
+        ch.range = voltageRange
+    
+        # Set coupling:
+        if coupling == "ACV":
+            ch.coupling = libtiepie.CK_ACV
+        else:
+            ch.coupling = libtiepie.CK_DCV
+    
+    dataList = []
+    
+    if autoRange == 1:
+        
+        scp.start()
+    
+        # Wait for measurement to complete:
+        while not (scp.is_data_ready or scp.is_data_overflow):
+            time.sleep(0.01)  # 10 ms delay, to save CPU time
+    
+        # Get data:
+        data = scp.get_data()
+        size = len(data[0])
+        #put data into an array (maybe I can skip this!)
+        dataArray = np.zeros( [size,3] )
+        dataArray[:,0] = np.linspace(0,acqTime,size) #Time
+        dataArray[:,1] = data[0] #Channel 1
+        dataArray[:,2] = data[1] #Channel 2
+    
+        rangeStdCH1 = np.std(data[0])
+        rangeStdCH2 = np.std(data[0])
+        # Stop stream:
+        scp.stop()
+        
+        autoRangeValue = max(rangeStdCH1,rangeStdCH1)*gainAutoRange
+        ch.range = autoRangeValue
+        print("Range set to "+str(autoRangeValue)+"V")
+        
+    
+    print("acquiring data")
+    
+    for n in tqdm(range(N)):    
+    
+        # Start measurement:
+        scp.start()
+    
+        # Wait for measurement to complete:
+        while not (scp.is_data_ready or scp.is_data_overflow):
+            time.sleep(0.01)  # 10 ms delay, to save CPU time
+    
+        # Get data:
+        data = scp.get_data()
+        size = len(data[0])
+        #put data into an array (maybe I can skip this!)
+        dataArray = np.zeros( [size,3] )
+        dataArray[:,0] = np.linspace(0,acqTime,size) #Time
+        dataArray[:,1] = data[0] #Channel 1
+        dataArray[:,2] = data[1] #Channel 2
+    
+        dataList.append(dataArray)
+    
+        # Stop stream:
+        scp.stop()
+    
+    print("saving data")
+    
+    for n in tqdm(range(N)):
+        
+        if n <= 9:
+            
+            outputFile = os.path.join(outputFolder,'0'+str(n))
+            np.save(outputFile,dataList[n])
+            
+        else:
+            
+            outputFile = os.path.join(outputFolder,str(n))
+            np.save(outputFile,dataList[n])
+    
+    
+    if rep != reps-1:
+        print("zzzzzzzzz")
+        time.sleep(delay)
+
+
+# Close oscilloscope:
+del scp
+beepy.beep(sound=1)
--- psdEvalTiepie.py
+++ psdEvalTiepie.py
@@ -1,90 +1,90 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 13 13:35:24 2022
+Created on Tue Dec 13 16:06:50 2022
 
-@author: Labq
+@author: tandeitnik
 """
+
 import os
 import numpy as np
-from scipy.optimize import curve_fit
-import scipy.signal as signal
 import matplotlib.pyplot as plt
+import scipy.signal as signal
 
-#ample_frequency = 50e2 #quantidade de pontos por traço
-#cq_time = 0.5 # tempo total do traço
-
-path = r"C:\Users\Labq\Dropbox\scripts\tiepie\data"  #pasta raíz aonde contém os dados
-N = 1 #quantos traços fazer por traço
-normalize = 1 #se 0 não normaliza o sinal do canal 1 com o canal 2, se 1 normaliza
-skip_points = 7 # número de pontos iniciais ignorados pro fit
+windows = 10 #number of windows used for the welch method
+channel = 1 #which channel to evaluate PSD
+pathFolders = r"C:\Users\Labq\Desktop\Daniel R.T\Nova pasta\data\forward-noparticle"  #root folder where the reps folders are saved
 
 
+folders = next(os.walk(pathFolders))[1]
+selectedFolders = folders #you want to ignore some folders, you can pass here a list of the desirable folders
 
 
-skip_line = 2
-files = next(os.walk(path))[2]
-data = []
+powerList = []
 
-PSD_list = []
+if 'f' in locals():
+    del f
 
-for file in files:
-
-    data = np.genfromtxt(path+'/'+file, delimiter=';', skip_header=skip_line)
+for folder in folders:
     
-    dt = data[1,1]
-    rowsPerTrace = int(len(data)/N)
-    
-    #calculando o PSD assumindo que os dados foram pegos numa QPD, que o sinalesteja no canal 1 e que a soma no canal 2
-    for i in range(N):
+    if folder in selectedFolders:
         
-        if normalize == 1:
-            freq, power = signal.welch(data[i*rowsPerTrace:(i+1)*rowsPerTrace,2]/data[i*rowsPerTrace:(i+1)*rowsPerTrace,3], 1/dt, window = 'rectangular', nperseg = rowsPerTrace)
-        else:    
-            freq, power = signal.welch(data[i*rowsPerTrace:(i+1)*rowsPerTrace,2], 1/dt, window = 'rectangular', nperseg = rowsPerTrace)
-        if i == 0:
+        path = os.path.join(pathFolders,folder)
+        files = next(os.walk(path))[2]
+        
+        data = []
+        
+        for file in files:
             
-            PSD  = power
+            if file == '.DS_Store':
+                
+                pass
             
-        else:
+            else:
+                
+                data.append(np.load(os.path.join(pathFolders,folder,file))[:,channel])
+                
+                if ('f' in locals()) == False:
+                
+                    f = 1/(np.load(os.path.join(pathFolders,folder,file))[1,0]-np.load(os.path.join(pathFolders,folder,file))[0,0])
+                   
+        freq, power = signal.welch(data[0], f, window = 'hamming', nperseg = int(len(data[0])/windows))
+        errors = 0
+    
+        for i in range(1,len(data)):
             
-            PSD  += power
+            freq, powerTemp = signal.welch(data[i], f, window = 'hamming', nperseg = int(len(data[i])/windows))
+            if len(powerTemp) != len(power):
+                errors += 1
+            else:
+                power += powerTemp
+            
+        power = power/(len(data)-errors)
+        
+        powerList.append(power)
     
-    #tirando a média
-    PSD = PSD/N
-    
-    PSD_list.append(PSD[skip_points:])
-    
-mean_psd = PSD_list[0]
+selectedFoldersPlot = folders
 
-for i in range(1,len(files)):
-    
-    mean_psd = mean_psd + PSD_list[i]
-    
-mean_psd = mean_psd/len(files)
+fig = plt.figure()
+plt.rcParams.update({'font.size': 14})
+plt.rcParams["axes.linewidth"] = 1
 
-var_psd = (PSD_list[0]-mean_psd)**2
+ax = plt.gca()
 
-for i in range(1,len(files)):
+for i, folder in enumerate(folders):
+        
     
-    var_psd = var_psd + (PSD_list[i]-mean_psd)**2
+    if folder in selectedFoldersPlot:
     
-var_psd = var_psd/len(files)
-      
-def aliased_lorentzian(f, D, f_c, cst ):                                       #Gives the function that replaces the Lorentzian in the case of finite sampling frequency, and it should fit the experimental spectrum for all frequencies of 0,f k<f Nyq if            # D is not a trustable value because it carries information about the conversion factor between position and milivolts units
-    global dt                                                                  #Sampling interval   
-    f_s=1/dt                                                                   #Sampling frequency     
-    c=np.exp(-2*np.pi*f_c/f_s)                                                 #Exponential that carries information about f_c
-    k=f*dt                                                                     #The frequencies in which PSD is evaluated multiplied by dt  
-    return (1-c**2)*D/(2*np.pi*f_c*f_s)*1/(1+c**2-2*c*np.cos(2*np.pi*k))+cst
-
-fit = curve_fit(aliased_lorentzian,freq[skip_points:],mean_psd)
-ans, cov = fit
-   
-D,f_c,cst = ans    
-perr = np.sqrt(np.diag(cov))        
-
-fig, ax = plt.subplots()
-ax.errorbar(freq[skip_points:], mean_psd, fmt='o', markersize=1, color='red', ecolor='red', elinewidth=0.5, capsize=0)
-ax.plot(freq[skip_points:],aliased_lorentzian(freq[skip_points:], D, f_c, cst ), color='blue')
-ax.set_xscale("log", nonposx='clip')
-ax.set_yscale("log", nonposy='clip')
+        #ax.scatter(freq ,powerList[i],label = folder , s = 10)
+        ax.set_xlim([100, f/2])
+        ax.set_ylim([min(powerList[i]), max(powerList[i])])
+        ax.plot(freq ,powerList[0],label = folder)
+        
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.legend()
+ax.set(ylabel=r'$V^2$/Hz')
+ax.set(xlabel=r'freq [Hz]')
+ax.grid(alpha = 0.4)
+        
